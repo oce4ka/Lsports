a1ff626608519f174992df6697024e34b90c3323
Marina Kotelnikov <oce4ka@gmail.com>
2023-12-08 00:13:04 +0100

    premium-racing.php

---
+++ theme/premium-racing.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Premium Racing
+ *
+ * The template for displaying Premium Racing category
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package LSport
+ */
+
+get_header();
+?>
+
+<?php get_footer();
