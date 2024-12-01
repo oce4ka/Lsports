bb932e18c15821fed2df47f8903442f8df138e1a
Marina Kotelnikov <oce4ka@gmail.com>
2024-12-01 12:21:49 +0200

    event carousel is common for events1, events2, events3 and event-ice posts

--- theme/event-item3.php
+++ theme/event-item3.php
@@ -2,21 +2,21 @@
 
 <section class="s-events-post">
     <div class="container">
-        
+
         <div class="s-events-post__wrapper">
             <?php if (get_field('image_without_video')): ?>
                 <div class="s-events-post__image2">
-       
+
 
 
 
 
         <div class="s-events-post__content">
-           
-                 <div style="display: flex;">      
-                    <div>  <h1><?php the_field('title'); ?></h1>   </div>    
+
+                 <div style="display: flex;">
+                    <div>  <h1><?php the_field('title'); ?></h1>   </div>
             </div>
-            
+
                     <div class="meet_us_info">
               <span class="time2">
                         <?php
@@ -26,46 +26,46 @@ else:
     echo date("F", strtotime(get_field('date_start', false, false))) . ' ' . date("j", strtotime(get_field('date_start', false, false))) . ' ' . date("Y", strtotime(get_field('date_start', false, false))) . ' at ' . get_field('time');
 endif;
 ?>
-                       
+
                     </span>
-                             
+
                     <span class="place3"><?php the_field('address') ?></span>
-                                  
+
                                     <span class="place2"><?php the_field('stand_location') ?></span>
-         
+
                 </div>
-            
+
 <div class="events-divider"></div>
 
                  <div class="meet_us_info2">
             <?php if (get_field('description')): ?>
-             
+
                 <?php the_field('description') ?>
             <?php endif; ?>
             <?php if (get_field('description')): ?>
-               
+
                 <?php the_field('schedule') ?>
             <?php endif; ?>
   </div>
-                    
-                    
+
+
         </div>
 
 
                 </div>
             <?php endif; ?>
 
-          
-            
+
+
                 <div class="s-events-post__detail2" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id(get_the_ID())); ?>)">
-            
+
             <div class="background-overlay-events-3">
-            </div>            
-            
-                    
             </div>
 
-   
+
+            </div>
+
+
     </div></div>
 </section>
 
@@ -181,78 +181,4 @@ endif;
     </div>
 </div>
 
-<?php
-$today = date("Ymd");
-$posts = get_posts(array(
-    'numberposts' => -1,
-    'post_type' => 'post',
-    'category_name' => 'events2',
-    'meta_query' => array(
-        array(
-            'key' => 'date_start',
-            'value' => $today,
-            'compare' => '>=',
-        ),
-    ),
-    'meta_key' => 'date_start',
-    'orderby' => 'meta_value_num',
-    'order' => 'ASC'
-));
-
-if ($posts): ?>
-    <section class="s-hp-news s-hp-news--event bg-white">
-        <div class="s-hp-news__headings">
-            <h2><?php the_field('next_events_decorated', 'option') ?></h2>
-            <a href="<?php the_field('see_all_events_link', 'option') ?>"><h6 class="arrow-after"><?php the_field('see_all_events', 'option') ?></h6></a>
-        </div>
-        <div class="s-hp-news__content">
-            <div class="s-hp-news__carousel-wrapper">
-                <div class="s-hp-news__carousel">
-                    <?php foreach ($posts as $post):
-                        setup_postdata($post);
-                        ?>
-                        <div class="s-hp-news__news-item">
-                            <div class="s-hp-news__news-item__content">
-                                <div class="image-wrapper" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>)"></div>
-                                <div class="s-hp-news__news-item__wrapper">
-                                    <div class="meta">
-                                        <div class="date">
-                                            <div class="icon-calendar"></div>
-                                            <?php
-                                            if (get_field('date_end')):
-                                                echo date("j", strtotime(get_field('date_start', false, false))) . '-' . date("j", strtotime(get_field('date_end', false, false))) . ' ' . date("F", strtotime(get_field('date_end', false, false))) . ', ' . date("Y", strtotime(get_field('date_end', false, false)));
-                                            else:
-                                                echo date("F", strtotime(get_field('date_start', false, false))) . ' ' . date("j", strtotime(get_field('date_start', false, false))) . ', ' . date("Y", strtotime(get_field('date_start', false, false)));
-                                            endif;
-                                            ?>
-                                        </div>
-                                        <div class="separator"></div>
-                                        <div class="place">
-                                            <div class="icon-place"></div>
-                                            <?php if (get_field('virtual')): ?>
-                                                <?php the_field('virtual', 'option'); ?>
-                                            <?php else: ?>
-                                                <?php the_field('location') ?>
-                                            <?php endif; ?>
-                                        </div>
-                                    </div>
-                                    <h3><?php the_field('title_short'); ?></h3>
-                                    <div class="excerpt"><?php the_field('excerpt'); ?></div>
-                                </div>
-                            </div>
-                            <a href="<?php the_permalink(); ?>" class="btn-read-more"><span class="text-vertical"><?php the_field('register_now', 'option'); ?></span></a>
-                        </div>
-                    <?php endforeach; ?>
-                </div>
-            </div>
-            <?php /*
-            <!--div class="s-hp-news__controls">
-                <div class="s-hp-news__arrow-prev arrow-after"></div>
-                <div class="s-hp-news__dots"></div>
-                <div class="s-hp-news__arrow-next arrow-after"></div>
-            </div-->
-            */ ?>
-        </div>
-    </section>
-    <?php wp_reset_postdata(); ?>
-<?php endif; ?>
+<?php include('event-carousel-part.php'); ?>
